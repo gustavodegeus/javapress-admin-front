f1633fdd3560aff15c22150826ca05e82e04619a
Gustavo de Geus <gustavodegeus@gmail.com>
2015-07-29 21:31:20 -0300

    Fix gh-pages deploy task

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('deploy', ['prod'], function () {
+gulp.task('deploy', ['prod'], function () { 
+  
   return gulp.src('./build/**/*')
     .pipe(ghPages());
 });
--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -3,19 +3,28 @@
 var config = require('../config');
 var gulp = require('gulp');
 var templateCache = require('gulp-angular-templatecache');
+var htmlreplace = require('gulp-html-replace');
+var gulpif = require('gulp-if');
 
 // Views task
 gulp.task('views', function () {
 
   // Put our index.html in the dist folder
   gulp.src('app/index.html')
+    //Replace <base> value to work in gh-pages
+    .pipe(gulpif(global.isProd, htmlreplace({
+      basetag: {
+        src: ['/javapress-admin-front/'],
+        tpl: '<base href="%s">'
+      }
+    })))
     .pipe(gulp.dest(config.dist.root));
 
   // Process any other view files from app/views
   return gulp.src(config.views.src)
     .pipe(templateCache({
-    standalone: true
-  }))
+      standalone: true
+    }))
     .pipe(gulp.dest(config.views.dest));
 
 });
